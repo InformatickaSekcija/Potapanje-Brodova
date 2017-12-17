e4302f56f5397ac3b6ca754af541c6381fc649cd
Administrator <liondj007@gmail.com>
2017-12-17 18:04:19 +0100

    Testing jquery way of solving the game field creation and click events

---
+++ PotapanjeBrodova/js/script2.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+   $('.podesi').click(function () {
+       var visina = $('.visina').val();
+       var sirina = $('.sirina').val();
+       var sir= $('.igra').width();
+       var vis= $('.igra').height();
+       
+       for(i=1; i <= sirina; i++){
+
+       }
+   })
+});

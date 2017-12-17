2f55b25b3941da25a4705f99db5facd8d0a68dbc
maxasrbenda <maxaserb@gmail.com>
2017-12-17 16:45:07 +0100

    uredjivanje izgleda ubacivanjem tabova

--- PotapanjeBrodova/js/script.js
+++ PotapanjeBrodova/js/script.js
@@ -44,14 +44,15 @@ function Klik(e)
     }
 }
 
-//funkcija kojom generišemo tabelu
-function ucitajTabelu() {
-    var sirina= prompt("Molim Vas unesite sirinu tabele u poljima", "10");
-    var visina= prompt("Molim Vas unesite visinu tabele u poljima", "10");
-    var div = document.getElementsByClassName('main');
-    div.innerHTML += makeTableHTML(sirina,visina);
-}
+    //funkcija kojom generišemo tabelu
+    function ucitajTabelu() {
+        var sirina= prompt("Molim Vas unesite sirinu tabele u poljima", "10");
+        var visina= prompt("Molim Vas unesite visinu tabele u poljima", "10");
+        var div = document.getElementsByClassName('main');
+        div.innerHTML += makeTableHTML(sirina,visina);
+    }
 
+    //funkcija koja generise brodove, ubacuje ih u niz
     function makeShips(s,v){
     var rand;
     while(Math.round(s*v/3)>brodovi.length){
@@ -60,8 +61,9 @@ function ucitajTabelu() {
             brodovi.push(rand);
         }
     }
-
     }
+
+    //funkcija...
     function makeTableHTML(s,v) {
         var result = "<table border=1>";
         var id=0;

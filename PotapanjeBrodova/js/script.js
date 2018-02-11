a5443c3e45d89e49d31bac669955ef7a39cb8a6d
vulekeva <vulekeva@gmail.com>
2018-02-11 18:49:55 +0100

    Dodao funkcije dodaj dvojku i dodaj pored, izbacio iz glavnog dodaj brodove pa trenutno dodaje samo jednu dvojku. Povećao broj poteza na mnogo veliki broj zbog testiranja

--- PotapanjeBrodova/js/script.js
+++ PotapanjeBrodova/js/script.js
@@ -13,9 +13,12 @@ function Klik(e)
             var sirina = Math.max(parseInt(prompt("Molim Vas unesite sirinu tabele u poljima", "5")), 5);
             var visina = Math.max(parseInt(prompt("Molim Vas unesite visinu tabele u poljima", "5")), 5);
             var div = e.target.parentNode;
+
             div.childNodes[3].innerHTML = makeTableHTML(sirina, visina);
-            makeShips(sirina, visina);
-            maxPoteza = Math.round(sirina * visina / 2) + 10;
+
+            // makeShips(sirina, visina);
+            ubaci2ku(brodovi,sirina,visina);
+            maxPoteza = Math.round(sirina * visina / 2) + 100;
             div.childNodes[5].innerHTML = 'Maksimalan broj poteza ' + maxPoteza;
             potez = 0;
             div.childNodes[7].innerHTML = 'Trenutni potez ' + potez;
@@ -67,7 +70,7 @@ function ucitajTabelu() {
 
     function makeTableHTML(s,v) {
         var result = "<table border=1>";
-        var id=0;
+        var id=-1;
         for(var i=1; i<=s; i++) {
             result += "<tr>";
             for(var j=1; j<=v; j++){
@@ -79,3 +82,40 @@ function ucitajTabelu() {
         result += "</table>";
         return result;
     }
+//ubacuje dvojku, dodati proveru za rand da li već postoji brod na toj poziciji
+    function ubaci2ku(niz,s,v) {
+        var rand,rand2,rez;
+        var ubaceno;
+        rand=Math.floor(Math.random() * s * v)+1;
+        niz.push(rand);
+        while (ubaceno == undefined) {
+            ubaceno=ubaciPored(rand,s,v,niz);
+        }
+        alert(rand+' '+ubaceno);
+    }
+//ubaci pored zadate pozicije, dodaje na poziciju pored u zavisnosti od izvucenog slucajnoh broja,
+//dodati da proverava da li je nešto ubačeno i ako nije da ponavlja dok ne ubaci
+        function ubaciPored(pozicija,s,v,niz){
+            var rand2=Math.floor(Math.random()*4)+1;
+            alert(rand2);
+            switch(rand2){
+                case 1:if((Math.floor((pozicija+1)/v)==Math.floor(pozicija/v))&&(niz.indexOf((pozicija+1)) == -1)){
+                    niz.push((pozicija+1));
+                    return 1;//ubaci desno
+                }
+                case 2:if((pozicija+v<s*v)&&(niz.indexOf(pozicija+v) == -1)){
+                    niz.push(pozicija+v);
+                    return 2;//ubaci dole
+                }
+                case 3:if((Math.floor((pozicija-1)/v)==Math.floor(pozicija/v))&&(niz.indexOf(pozicija+v) == -1)){
+                    niz.push((pozicija-1));
+                    return 3;//ubaci levo
+                }
+                case 4:if((pozicija-v>=0)&&(niz.indexOf(pozicija+v) == -1)){
+                    niz.push(pozicija-v);
+                    return 4;//ubaci gore
+                }
+            }
+        }
+
+
